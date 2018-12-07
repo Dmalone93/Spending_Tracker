4ad9d423bd90548becf1634ead0179c2a6d5768f
Declan Malone <declanamalone@gmail.com>
2018-12-07 12:32:11 +0000

    got pry working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,22 @@ merchant1 = Merchant.new ({
   "merchant_name" => "Amazon"
   })
 
-merchant1.save()
+  merchant1.save()
+
+merchant2 = Merchant.new ({
+  "merchant_name" => "Morrisons"
+  })
+
+  merchant2.save()
+
+merchant3 = Merchant.new ({
+  "merchant_name" => "Jiu Jitsu"
+  })
+
+  merchant3.save()
+
+
+
+
+binding.pry
+nil
