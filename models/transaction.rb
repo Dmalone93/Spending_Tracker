bbda4c206b88a8ae02bcf132ba880bb40cfe6e60
Declan Malone <declanamalone@gmail.com>
2018-12-07 14:57:55 +0000

    Added Find function for Transaction Class git push

--- models/transaction.rb
+++ models/transaction.rb
@@ -5,7 +5,7 @@ class Transaction
   attr_reader :id, :merchant_id, :tag_id
 
   def initialize(options)
-    @id = options['id'].to if options['id']
+    @id = options['id'].to_i if options['id']
     @merchant_id = options['merchant_id']
     @tag_id = options['tag_id']
   end
@@ -24,6 +24,19 @@ class Transaction
     SqlRunner.run(sql)
   end
 
+
+  def self.all()
+    sql = "SELECT * FROM transactions"
+    results = SqlRunner.run(sql)
+    return results.map {|transaction| Transaction.new(transaction)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM transactions WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Transaction.new(results.first)
+  end
 
 
 
