e6482c35b8a102844b52dd8f84a87b43ea126634
Declan Malone <declanamalone@gmail.com>
2018-12-07 13:50:09 +0000

    finished basic functions on Merchant Class and started Tag Class(save inc)

--- models/merchant.rb
+++ models/merchant.rb
@@ -1,16 +1,46 @@
+require_relative( '../db/sql_runner.rb' )
+
 class Merchant
+
+  attr_reader :id
+  attr_accessor :merchant_name
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @merchant_name = merchant_name['merchant_name']
+    @merchant_name = options['merchant_name']
   end
 
   def save()
-    sql = "INSERT INTO merchants (merchant_name) VALUES ($1) RETURNING id"
+    sql = "INSERT INTO merchants
+    (merchant_name)
+    VALUES ($1)
+    RETURNING id"
     values = [@merchant_name]
     results = SqlRunner.run(sql, values)
-    @id = results.first['id'].to_i
+    @id = results.first()['id'].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM merchants"
+    results = SqlRunner.run(sql)
+    return results.map{|merchant| Merchant.new(merchant)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM merchants"
+    results = SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM merchants WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Merchant.new(results.first)
+  end
+
+
+
+
 
 
 end
---
+++ models/tag.rb
@@ -0,0 +1,24 @@
+require_relative( '../db/sql_runner.rb' )
+
+class Tag
+
+  attr_reader :id
+  attr_accessor :tag_name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @tag_name = options['tag_name']
+  end
+
+  def save()
+    sql = "INSERT INTO tags
+    (tag_name)
+    VALUES ($1)
+    RETURNING id"
+    values = [@tag_name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+
+end
